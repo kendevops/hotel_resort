844aab4fdeaf69a2de7ce93aa4d77ab5b9f624b1
Kenneth Aladi <kennethmichael11@gmail.com>
2020-05-28 17:33:21 +0100

    Created the basic components needed for the project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import Home from "./Components/Home";
+import Rooms from "./Components/Rooms";
+import SingleRoom from "./Components/SingleRoom";
+import Error from "./Components/Error";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Home />
+      <Rooms />
+      <SingleRoom />
+      <Error />
+    </>
+  );
 }
 
 export default App;
